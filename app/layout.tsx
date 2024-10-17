aacefee2796ffe66ab890d43e35d48b559af6ddd
Gregory <gregory>
2024-10-18 00:00:51 +0200

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,12 @@ import SideNav from "@/app/ui/dashboard/sidenav";
 export default function RootLayout({children,}: { children: React.ReactNode; }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
-      <div className="navBar"><SideNav/></div>
+    <head>
+        <title>Portfolio</title>
+        <meta name="description" content="Portfolio de BROULIN Grégoryg"/>
+    </head>
+    <body className={`${inter.className} antialiased`}>
+    <div className="navBar"><SideNav/></div>
       <div className="content">{children}</div>
       </body>
     </html>
