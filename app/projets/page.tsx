33ab53bad7382db2642e4851c93402421be14ab7
Gregory <gregory>
2024-10-18 09:34:31 +0200

    webp

--- app/projets/page.tsx
+++ app/projets/page.tsx
@@ -6,14 +6,15 @@ export default function Page() {
     <div className="cardWrapper">
       <div className="cardProject">
         <div className="imgProject">
-          <img src="/img/project/flatcraft.jpg" alt="project"/>
+          <img src="/img/project/flatcraft.webp" alt="project"/>
         </div>
         <div className="textSkill">
           <h3>Flatcraft [2023-2024] - [Java]</h3>
           <p>
             Dans ce projet, j'ai réalisé avec les membres de mon équipe le jeu Flatcraft inspiré du jeu Minecraft
             ou encore Terraria en Java.
-            <br/>
+          </p>
+          <p>
             Ce projet m'a permis d'apprendre et de comprendre les patrons de conceptions en Java, mais aussi
             d'approfondir mes compétence en programmation orientée objet.
           </p>
@@ -21,7 +22,7 @@ export default function Page() {
       </div>
       <div className="cardProject">
         <div className="imgProject">
-          <img src="/img/project/ray-tracing.jpg" alt="project"/>
+          <img src="/img/project/ray-tracing.webp" alt="project"/>
         </div>
         <div className="textSkill">
           <h3>Ray tracing [octobre 2023] - [Java]</h3>
@@ -30,7 +31,8 @@ export default function Page() {
             a pour but de générer, à partir d'un fichier texte, une image composée de formes géométriques
             (triangles et sphères) et des plans. La génération de l'image prend en compte les lumières, les ombres
             générés par les objets et d'autres fonctionnalités.
-            <br/>
+          </p>
+          <p>
             Ce projet m'a permis d'approfondir mes connaissances pour les patrons de conceptions en Java et en
             programmation orientée objet.
           </p>
@@ -38,7 +40,7 @@ export default function Page() {
       </div>
       <div className="cardProject">
         <div className="imgProject">
-          <img src="/img/project/jo.jpg" alt="project"/>
+          <img src="/img/project/jo.webp" alt="project"/>
         </div>
         <div className="textSkill">
           <h3>JO 2024 [2023-2024] - [PHP | Laravel]</h3>
@@ -46,7 +48,8 @@ export default function Page() {
             Réalisé avec le framework Laravel, ce projet à tout d'abord eu pour but d'afficher une liste de sports
             basés sur la liste des sports des Jeux Olympiques 2024, ensuite j'ai ajouté les athlètes et un classement
             de ces athlètes dans un jeu.
-            <br/>
+          </p>
+          <p>
             Grâce a ce projet, j'ai découvert le framerwork Laravel, ainsi que certains aspects de ce framework
             comme le routage CRUD (Create, Read, Update, Delete) appliqué pour les sports et les athlètes,
             l'authentification et la gestion d'accès, et enfin les relations entre tables.
@@ -55,7 +58,7 @@ export default function Page() {
       </div>
       <div className="cardProject">
         <div className="imgProject">
-          <img src="/img/project/marathon.jpg" alt="project"/>
+          <img src="/img/project/marathon.webp" alt="project"/>
         </div>
         <div className="textSkill">
           <h3>Marathon du web [décembre 2023] - [PHP | Laravel]</h3>
@@ -64,7 +67,8 @@ export default function Page() {
             Durant 2 journées nous avions chacun des tâches à réaliser, la nôtre était de concevoir un site
             référençant une liste d'histoires composées de chapitres, et qu'un utilisateur connecté puisse lire une
             des histoires, mais aussi qu'il puisse créer lui-même son histoire.
-            <br/>
+          </p>
+          <p>
             Cet évènement m'a permis de consolider mes compétences avec le framework Laravel et en
             base de données et de découvrir le travail de groupe avec d'autres membres que des étudiants
             du départements informatique, en particulier avec le rôle d'intégrateur pour ce projet.
